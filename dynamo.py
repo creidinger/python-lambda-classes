6997f130e04f9e724d14147e47a19a6eb4f9f6e3
Chase Reidinger <chase.reidinger@adepdev.com>
2022-11-03 17:12:31 -0400

    refactored dynamo to make it more mature

--- dynamo.py
+++ dynamo.py
@@ -1,150 +1,174 @@
 import datetime
 import uuid
+import json
 # 3rd party imports
 import boto3
-from boto3.dynamodb.conditions import Key
+from boto3.dynamodb.conditions import Key, Attr
 
 
 class Dynamo:
-    """DynamoDB functions for lambda"""
+    """DynamoDB functions for lambda
 
-    def __init__(self, settings, logger):
+    docs: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb.html
+    """
+
+    def __init__(self, logger):
         """
         args:
-            - settings (obj): import settings using the singleton pattern
             - logger (obj): import logger object using the singleton pattern
         """
-        self.dynamodb = boto3.resource(
-            'dynamodb')    # The type of client we're using
-        self.prod_table_name = settings.dynamodb_table
-        self.staging_table_name = settings.dynamodb_table_staging
+        self.client = boto3.resource(
+            'dynamodb')
+        self.table = None
         self.logger = logger
 
-    def dynamo_get_active_table(self, use_production):
-        """ Determine which environment to use
+    def set_table(self, table_name):
+        """ Set the table that we want to work with
+
         args:
-            - use_production (bool): Tells which environment we're working in
+            - table_name (str): the table we're writing data to
         """
-        # determine which env to store data
-        if use_production:
-            return self.prod_table_name
+        self.table = self.client.Table(table_name)
 
-        return self.staging_table_name
-
-    def dynamo_get(self, data, use_production):
+    def dynamo_get_all(self):
         """
-        get and item from DynamoDb
-        args:
-            - data (json): the data received by the lambda function
-            - use_production (bool): Tells which environment we're working in
+        Get all items from the DB.
         """
-        self.logger.info('Dynamo.dynamo_get: Checking if the user exists.')
 
-        # get the table and init
-        active_table = self.dynamo_get_active_table(
-            use_production=use_production)
-        table = self.dynamodb.Table(active_table)
-        item = None
+        self.logger.info('Dynamo.dynamo_get_all: start')
 
         try:
-            response = table.get_item(
-                Key={
-                    'email': data['email'],
-                }
-            )
+            # get all items
+            items = self.table.scan()
+            self.logger.info(f'Dynamo.dynamo_get_all: success')
+            return(items)
+
         except Exception as e:
-            self.logger.error('Dynamo.dynamo_get: Unable to table.get_item()')
-            self.logger.error(f'Dynamo.dynamo_get: {e}')
-            # return empty if error
-            # this will allow us to try to save the data
+            # display failure message and return failure status
+            self.logger.error(f'Dynamo.dynamo_get_all: failed...\n\n\n {e}')
+
+            return {
+                "statusCode": 500,
+                "body": json.dumps({"message": "Unable to get items"}),
+            }
+
+    def dynamo_get_all_by_status(self, status):
+        """
+        Get all items with the provided status.
+        args:
+            - status (str): The status we're filtering for
+        """
+
+        self.logger.info(f'Dynamo.dynamo_get_all_by_status: status {status}')
+
+        try:
+            # get all items
+            items = self.table.scan(FilterExpression=Attr('status').eq(status))
+            self.logger.info(f'Dynamo.dynamo_get_all_by_status: success')
+            return(items)
+
+        except Exception as e:
+            # display failure message and return failure status
+            self.logger.error(
+                f'Dynamo.dynamo_get_all_by_status: failed...\n\n\n {e}')
+
+            return {
+                "statusCode": 500,
+                "body": json.dumps({"message": "Unable to get approved items"}),
+            }
+
+    def dynamo_post(self, data):
+        """
+        Add a new item to the DB
+        args:
+            - data (json): the item we're adding to the db
+        """
+
+        self.logger.info('Dynamo.dynamo_post: start')
+
+        try:
+            item = self.table.put_item(Item=data)
+            self.logger.info(f'Dynamo.dynamo_post: success')
+            return(item)
+
+        except Exception as e:
+            # display failure message and return failure status
+            self.logger.error(f'Dynamo.dynamo_post: failed...\n\n\n {e}')
+
+            return {
+                "statusCode": 500,
+                "body": json.dumps({"message": "Unable to PUT items."}),
+            }
+
+    def dynamo_get_item(self, uid):
+        """
+        get a single item from DynamoDb
+        args:
+            - uid (str): The ID of the item we're looking for
+        """
+
+        self.logger.info('Dynamo.dynamo_get_item: start')
+
+        try:
+            item = self.table.get_item(Key={'uid': uid})
+            self.logger.info('Dynamo.dynamo_get_item: end.')
             return item
 
-        item = response["Item"]
-        self.logger.info('Dynamo.dynamo_get: end.')
-        return item
-
-    def dynamo_update_item(self, data, use_production):
-        """
-        Update an existing user DynamoDb
-        args:
-            - data (json): the data received by the lambda function
-            - use_production (bool): Tells which environment we're working in
-        """
-
-        self.logger.info(
-            'Dynamo.dynamo_update_item: dynamodb.table.update_item: Start.')
-        # get the table and init
-        active_table = self.dynamo_get_active_table(
-            use_production=use_production)
-        table = self.dynamodb.Table(active_table)
-
-        try:
-            table.update_item(
-                Key={
-                    'email': data['email']
-                },
-                UpdateExpression="set #contact_name=:n, phone=:p, email=:e, messsage=:m, edit_date=:d",
-                ExpressionAttributeValues={
-                    ':n': data["name"],
-                    ':p': data["phone"],
-                    ':e': data["email"],
-                    ':m': data["message"],
-                    ':d': str(datetime.datetime.now())
-                },
-                # Doc below explains how to handle updating a table
-                # that contains a reserved name
-                # https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.ExpressionAttributeNames.html
-                ExpressionAttributeNames={
-                    "#contact_name": "name"
-                }
-            )
         except Exception as e:
-            self.logger.info(
-                'Dynamo.dynamo_update_item: dynamodb.table.update_item: end.')
             self.logger.error(
-                f'Dynamo.dynamo_update_item: dynamodb.table.update_item: {e}')
-            return False
-        else:
-            self.logger.info(
-                'Dynamo.dynamo_update_item: dynamodb.table.update_item: Success!!!')
-            return True
+                f'Dynamo.dynamo_get_item: Unable to table.get_item() \n\n\n{e}')
 
-    def dynamo_put(self, data, use_production):
+            # return empty if error
+            # this will allow us to try to save the data
+            return {
+                "statusCode": 500,
+                "body": json.dumps({"message": f"Unable to Get item. ID: {uid}"}),
+            }
+
+    def dynamo_put_item(self, data):
         """
         Upload data to DynamoDb
         args:
             - data (json): the data received by the lambda function
-            - use_production (bool): Tells which environment we're working in
         """
-        self.logger.info(
-            'Dynamo.dynamo_put: uploading event data to DynamoDb...')
-
-        # get the table and init
-        active_table = self.dynamo_get_active_table(
-            use_production=use_production)
-        table = self.dynamodb.Table(active_table)
+        self.logger.info('Dynamo.dynamo_put_item: start')
 
         # create a new item (row)
         # source: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#creating-a-new-item
 
         try:
-            table.put_item(
-                Item={
-                    # generate a ramdom/unique ID for each DB Entry
-                    'id': str(uuid.uuid4()),
-                    'name': data['name'],
-                    'phone': data['phone'],
-                    'email': data['email'],
-                    'message': data['message'],
-                    'create_date': str(datetime.datetime.now()),
-                    'edit_date': None,
-                }
-            )
+            item = self.table.put_item(Item=data)
+            self.logger.info(f'Dynamo.dynamo_put_item: success')
+            return(item)
+
         except Exception as e:
             self.logger.error(
-                f'Dynamo.dynamo_put: dynamodb.table.put_item: {e}')
-            return False
-        else:
-            self.logger.info(
-                'Dynamo.dynamo_put: dynamodb.table.put_item: Success!!!')
-            return True
+                f'Dynamo.dynamo_put_item: dynamodb.table.put_item: {json.dumps(e, indent=4)}')
+            return {
+                "statusCode": 500,
+                "body": json.dumps({"message": "Unable to PUT item."}),
+            }
+
+    def dynamo_delete_item(self, uid):
+        """
+        Upload data to DynamoDb
+        args:
+            - uid (str): The ID of the item we're looking for
+        """
+        self.logger.info('Dynamo.dynamo_delete_item: start')
+
+        # create a new item (row)
+        # source: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#creating-a-new-item
+
+        try:
+            item = self.table.delete_item(Key={'uid': uid})
+            self.logger.info(f'Dynamo.dynamo_delete_item: success')
+            return(item)
+
+        except Exception as e:
+            self.logger.error(
+                f'Dynamo.dynamo_delete_item: dynamodb.table.put_item: {json.dumps(e, indent=4)}')
+            return {
+                "statusCode": 500,
+                "body": json.dumps({"message": "Unable to DELETE item."}),
+            }
