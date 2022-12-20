61cb32fee03f26e305aaf868180e44f639721213
Chase Reidinger <chase.reidinger@adepdev.com>
2022-12-20 03:05:49 -0500

    API integrations updated

---
+++ OpusHealth/OPUS_health.py
@@ -0,0 +1,108 @@
+import os
+import json
+# 3rd part imports
+import requests
+import xmltodict
+
+
+class OpusHealthPartnerInterface:
+    """The OPUS Health Partner Interface web service will provide external partners with the ability to communicate with Alternative Sampling platform in real time:"""
+
+    def __init__(self, logger):
+        """
+
+        Args:
+            - logger (obj): import logger object using the singleton pattern
+        """
+        self.logger = logger
+        self.headers = {
+            "Content-Type": "text/xml;charset=utf-8",
+            "Accept-Encoding": "gzip,deflate"
+        }
+        self.url = None
+        self.payload = None
+        self.user = None
+        self.password = None
+        self.groupid = None
+
+    def set_meta(self, url, user, password, groupid):
+        """_summary_
+
+        Args:
+            - url (str): The API endponit we're sending the request to
+            - user (str): The user account used the authenicate in the API
+            - password (str): The password used the authenicate in the API
+            - groupid (str): The group ID we're using to create the ID
+        """
+
+        try:
+            self.url = url
+            self.user = user
+            self.password = password
+            self.groupid = groupid
+
+        except Exception as e:
+            self.logger.error(f"OpusHealthPartnerInterface.set_meta: {e}")
+            return False
+
+        return True
+
+    def set_payload(self):
+        """Creates the payload for the api request"""
+
+        # build the XML payload
+        self.payload = f"""
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:par="http://www.tripleiesampling.com/PartnerInterfaceWS/">
+        <soapenv:Header>
+            <par:ESamplingSoapHeader>
+                <par:UserName>{self.user}</par:UserName>
+                <par:Password>{self.password}</par:Password>
+            </par:ESamplingSoapHeader>
+        </soapenv:Header>
+        <soapenv:Body>
+            <par:GetNextAvailableDocumentNumber>
+                <par:groupNumber>{self.groupid}</par:groupNumber>
+            </par:GetNextAvailableDocumentNumber>
+        </soapenv:Body>
+    </soapenv:Envelope>"""
+
+        self.logger.info(
+            f'OpusHealthPartnerInterface.set_payload: xmldata: {self.payload}')
+
+    def post_request(self):
+        """Send a request to the OPUS API
+
+        Returns:
+            XML: returns and xml object 
+        """
+
+        self.set_payload()
+
+        try:
+            r = requests.post(
+                self.url, data=self.payload, headers=self.headers)
+            self.logger.info(
+                f'OpusHealthPartnerInterface.post_request: API response status code: {r.status_code}')
+
+        except Exception as e:
+            self.logger.error(
+                f'OpusHealthPartnerInterface.post_request: API failed: \n\n {e}')
+
+        return r
+
+    def get_document_number(self, xml_str):
+        """Extract the document number from the xml data that was recevied from the request
+
+        Args:
+            xml_str (str): Data is received as a string
+        """
+
+        # convert response xml to dict
+        xml_response = xmltodict.parse(xml_str)
+
+        self.logger.info(
+            f'OpusHealthPartnerInterface.get_document_number: xml_response \n{json.dumps(xml_response, indent=4)}')
+
+        # get the document number
+        return xml_response["soap:Envelope"]["soap:Body"]["GetNextAvailableDocumentNumberResponse"][
+            "GetNextAvailableDocumentNumberResult"]["DocumentNumber"]
---
+++ aws/s3.py
@@ -0,0 +1,80 @@
+import os
+# 3rd part imports
+import boto3
+from botocore.exceptions import ClientError
+
+
+class S3:
+    """AWS Simple Email Service functions for lambda
+
+    docs: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html
+    """
+
+    def __init__(self, logger):
+        """
+
+        Args:
+            - logger (obj): import logger object using the singleton pattern
+        """
+        self.logger = logger
+        self.region_name = "us-east-1"  # default value
+        self.client = None
+
+    def set_region(self, region_name):
+        """Set the region we will be using to send emails
+
+        Args:
+            - region_name (str): The region we want to change too
+        """
+
+        self.logger.info('S3.set_region: start')
+
+        self.region_name = region_name
+
+        self.logger.info(
+            f'S3.set_region: end - using region {self.region_name}')
+
+    def set_s3_client(self, ACCESS_KEY, SECRET):
+        """Init the s3 client
+
+        Args:
+            ACCESS_KEY (str): The access key for this bucket
+            SECRET (str): The secret key for this bucket
+        """
+
+        self.logger.info('S3.set_s3_client: start')
+
+        try:
+            self.client = boto3.client(
+                's3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET)
+
+        except Exception as e:
+            self.logger.error(f'S3.set_s3_client: {e}')
+
+        self.logger.info('S3.set_s3_client: end')
+
+    def upload_to_butcket(self, file_name, bucket, object_name):
+        """Upload an object to a bucket
+        source: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
+
+
+        Args:
+            - file_name (str): The file we're uploading
+                Ex: my_file.pdf
+            - bucket (str): Bucket to upload to
+            - object_name (str): S3 object name.
+        """
+
+        self.logger.info("S3.upload_to_butcket: start...")
+
+        try:
+            response = self.client.upload_file(file_name, bucket, object_name)
+
+        except ClientError as e:
+            self.logger.error(
+                f'upload_to_s3: S3 upload failed \n{e}')
+            self.logger.error('upload_to_s3: Exit')
+            return False
+
+        self.logger.info("S3.upload_to_butcket: Success - end...")
+        return True
